7d2c43310e482b803c1e7add022b0baf3adeee2d
gvacost <gonza.96.acosta@gmail.com>
2023-08-12 09:54:30 -0300

    satchanges

---
+++ products.js
@@ -0,0 +1,12 @@
+let prod = document.getElementById('products');
+prod.addEventListener('click')
+
+let clickEvent = (prod) =>{
+    if (prod=='BEBEDEROS Y BUZONES'){
+        async function getBB(){
+            try{
+                const Url =  
+            }
+        }
+    }
+}
